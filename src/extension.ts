455f1bb2b8f33599ac8ab49ab3b1ad369f0f32a2
Andrii Shupta <Andrii_Shupta@epam.com>
2020-12-19 01:40:56 +0200

    (feature) return of file explorer approach

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 
 import * as vscode from 'vscode';
 
-import { DepNodeProvider, Dependency } from './nodeDependencies';
+import { FileSystemProvider } from './fileExplorer';
 
 const editor = vscode.window.activeTextEditor;
 
@@ -22,12 +22,8 @@ async function saveComponent() {
 export function activate(context: vscode.ExtensionContext) {
 
 	// Samples of `window.registerTreeDataProvider`
-	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath);
-	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
-	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('extension.saveComponent', () => saveComponent());
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
-	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
-	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
+	const fileSystemProvider = new FileSystemProvider(`${vscode.workspace.rootPath}/src/shared`);
+	vscode.window.registerTreeDataProvider('fileExplorer', fileSystemProvider);
+	vscode.commands.registerCommand('extension.saveComponent', saveComponent);
+	vscode.commands.registerCommand('fileExplorer.openFile', vscode.window.showTextDocument);
 }
