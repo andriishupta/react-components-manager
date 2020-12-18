6bbd9b85d566e184a97699ba8e3da72e7cd38f0d
Andrii Shupta <Andrii_Shupta@epam.com>
2020-12-18 23:35:13 +0200

    (chore) vscode extension ivestigation: removal of unsused code; creation of saveComponent command

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,21 @@
 import * as vscode from 'vscode';
 
 import { DepNodeProvider, Dependency } from './nodeDependencies';
-import { JsonOutlineProvider } from './jsonOutline';
-import { FtpExplorer } from './ftpExplorer';
-import { FileExplorer } from './fileExplorer';
-import { TestView } from './testView';
+
+const editor = vscode.window.activeTextEditor;
+
+async function saveComponent() {
+	const text = editor.document.getText(editor.selection);
+
+	// User Input to name Gist file
+	const componentName = await vscode.window.showInputBox({
+		placeHolder: "Component's name"
+	});
+
+	console.log(componentName);
+
+	// fs.writeFileSync(`./${componentName}.jsx`, 'test');
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -15,21 +26,8 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
 	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
 	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
+	vscode.commands.registerCommand('extension.saveComponent', () => saveComponent());
 	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
 	vscode.commands.registerCommand('nodeDependencies.editEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
 	vscode.commands.registerCommand('nodeDependencies.deleteEntry', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
-
-	const jsonOutlineProvider = new JsonOutlineProvider(context);
-	vscode.window.registerTreeDataProvider('jsonOutline', jsonOutlineProvider);
-	vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
-	vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
-	vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
-	vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
-
-	// Samples of `window.createView`
-	new FtpExplorer(context);
-	new FileExplorer(context);
-
-	// Test View
-	new TestView(context);
 }
